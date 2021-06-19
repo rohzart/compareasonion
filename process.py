01ac8e2d8b23ff4397230c8a566f9cc9ee0a5d67
rohzart <rohzart@gmail.com>
2021-06-20 05:05:35 +0530

    init

---
+++ process.py
@@ -0,0 +1,63 @@
+import imgkit
+import json
+from urllib.parse import urlparse
+import os
+
+with open('compareasonion.config.json', 'r') as fp:
+    config = json.load(fp)
+
+options = config["imgkit"]["options"]
+# additional options: 'width': 400, 'disable-smart-width': ''
+
+alloweddomains = config["alloweddomains"]
+
+imgkitconfig = imgkit.config(
+    wkhtmltoimage=config["imgkit"]["wkhtmltoimagepath"])
+
+with open(config["inputfilepath"], 'r') as fp:
+    records = json.load(fp)
+
+records_json = {}
+for page_name in records:
+    print(f'Processing: {page_name}')
+    counter = 0
+    urls_json = []
+    for url in records[page_name]:
+        domain = ''
+        image_path = ''
+        error = ''
+        if url == '':
+            error = 'error: no page url provided'
+        try:
+            result = urlparse(url)
+        except Exception as e:
+            print(e)
+            error = f'error: urlparse > {url}'
+        domain = result.netloc
+        if not domain in alloweddomains:
+            error = f'error: invalid domain: {domain}'
+        if error == '':
+            # todo: if result.netloc is not included in allowed domains return skip generation
+            if all([result.scheme, result.netloc, result.path]):
+                counter = counter + 1
+                page_name = page_name.replace(' ', '_')
+                file_name = f'/{config["outputdir"]}/{page_name}_{str(counter)}.png'
+                if not os.path.exists(file_name):
+                    try:
+                        imgkit.from_url(url, file_name, options=options, config=imgkitconfig)
+                        image_path = file_name
+                    except Exception as e:
+                        if os.path.exists(file_name):
+                            os.remove(file_name)
+                        error = f'error: imgkit > {e}'
+            else:
+                error = 'error: invalid image url'
+
+        urls_json.append(
+            {"domain": domain, "image_path": image_path, "error": error})
+    records_json[page_name] = urls_json
+
+with open(config["outputjsonpath"], 'w') as outfile:
+    json.dump(records_json, outfile)
+
+print('---OVER---')
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Docs
+
+## Install
+
+### wkhtmltopdf
+
+```
+sudo apt-get install wkhtmltopdf
+```
+
+### imgkit : wrapper for wkhtmltopdf
+
+```
+pip install imgkit
+```
+
+## References
+* https://wkhtmltopdf.org/usage/wkhtmltopdf.txt
+* https://pypi.org/project/imgkit/
+* https://www.mankier.com/1/wkhtmltoimage
